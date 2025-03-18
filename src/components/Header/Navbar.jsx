2f8428766932d14850f889e13eb95c9b71ba6ecc
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-19 03:08:50 +0600

    added two more file

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,7 +1,7 @@
 import './Navbar.css';
 const Navbar = () => {
     return (
-        <div className='style'>
+        <div className='style border-b-1 pb-2'>
             <p className='font-bold'>E-Mart</p>
             <ul className='flex justify-between gap-4 font-bold'>
                 <li>Home</li>
