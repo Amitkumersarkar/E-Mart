55d288566b9671e131826b97ca8d7fc13d569acc
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-20 22:37:05 +0600

    added little update

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -3,7 +3,7 @@ const CartContainer = () => {
     return (
         <div>
             <h1>Cart Container</h1>
-            <div className='flex justify-between gap-4 cursor-pointer'>
+            <div className='flex justify-between gap-4 pt-1 cursor-pointer'>
                 <button className='btn'>Cart</button>
                 <div className=' btn'>
                     <button>About</button>
