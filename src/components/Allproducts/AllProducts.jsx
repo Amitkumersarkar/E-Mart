c55876a215360dd253e28065b9db34779a3da55b
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-22 03:18:04 +0600

    added new file

--- src/components/Allproducts/AllProducts.jsx
+++ src/components/Allproducts/AllProducts.jsx
@@ -1,5 +1,15 @@
+import { useEffect, useState } from 'react';
 import './AllProducts.css';
 const AllProducts = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('fakeData.json')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
+
     return (
         <div>
             <h1 className='font-bold text-black'>All Products</h1>
